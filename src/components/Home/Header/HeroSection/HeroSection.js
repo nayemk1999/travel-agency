a5c64b1eeb2901789891034d6c12b3cb13de9cba
Nayem Khan <nayemk900@gmail.com>
2021-08-16 01:18:20 +0600

    all device responsive

--- src/components/Home/Header/HeroSection/HeroSection.js
+++ src/components/Home/Header/HeroSection/HeroSection.js
@@ -2,14 +2,13 @@ import React from 'react'
 import { Card } from 'react-bootstrap'
 import HeaderBg from '../../../img/HeaderBody.png'
 import './HeroSection.css'
-// import "@fontsource/great-vibes"
 
 export default function HeroSection() {
     return (
-        <Card >
-            <Card.Img style={{ height: '800px' }} src={HeaderBg} alt="Card image" />
-            <Card.ImgOverlay className=" d-flex text-center align-items-center justify-content-center">
-                <div className='card-content p-5 text-white'>
+        <Card style={{zIndex: '-1'}} >
+            <Card.Img style={{ height: '500px' }} src={HeaderBg} alt="Card image" />
+            <Card.ImgOverlay className="d-flex text-center align-items-center justify-content-center">
+                <div className='card-content p-4 text-white'>
                     <h4 >Book Now</h4>
                     <h3>Best Rate Guarantee</h3>
                     <p>We guarantee ​you’ll find the lowest price here.</p>
--- src/components/Home/SearchBar/SearchBar.js
+++ src/components/Home/SearchBar/SearchBar.js
@@ -1,9 +1,47 @@
 import React from 'react'
+import { Col, Container, Form, Row } from 'react-bootstrap'
 import './SearchBar.css'
 export default function SearchBar() {
     return (
-        <>
-            <div class="search-container container" aria-label="Mini form">
+        <Container style={{zIndex: '5 !important', marginTop: '-50px'}}>
+            <Form>
+                <Row className='checkRate w-75 d-flex text-center align-items-center justify-content-center mx-auto'>
+                    <Col md>
+                        <select
+                            className="form-select m-2 "
+                            aria-label="Default select example"
+                        >
+                            <option selected>Check In</option>
+                        </select>
+                    </Col>
+                    <Col md>
+                        <select
+                            className="form-select m-2"
+                            aria-label="Default select example"
+                        >
+                            <option selected>Check Out</option>
+                        </select>
+                    </Col>
+                    <Col md>
+                        <select
+                            className="form-select m-2"
+                            aria-label="Default select example"
+                        >
+                            <option selected>Guest</option>
+                        </select>
+                    </Col>
+                    <Col md>
+                        <button className="check-btn m-2" type="submit">CHECK RATES</button>
+                    </Col>
+                </Row>
+            </Form>
+        </Container>
+
+
+
+    )
+}
+{/* <div class=" container" aria-label="Mini form">
                 <form >
                     <div className="d-flex align-items-center justify-content-center checkRate">
                         <select
@@ -11,7 +49,6 @@ export default function SearchBar() {
                             aria-label="Default select example"
                         >
                             <option selected>Check In</option>
-
                         </select>
                         <select
 
@@ -19,10 +56,8 @@ export default function SearchBar() {
                             aria-label="Default select example"
                         >
                             <option selected>Check Out</option>
-
                         </select>
                         <select
-
                             className="form-select me-2"
                             aria-label="Default select example"
                         >
@@ -31,7 +66,4 @@ export default function SearchBar() {
                         <button className="check-btn " type="submit">CHECK RATES</button>
                     </div>
                 </form>
-            </div>
-        </>
-    )
-}
+            </div> */}
